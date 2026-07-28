0fcbab60e2523c29dadda97b3274c11dc27afea4
Irenegathoni <irenegnjogu003@gmail.com>
2026-07-28 12:41:49 +0300

    Initial EduKonnect setup

---
+++ src/test/java/com/edukonnect/edukonnect_portal/EdukonnectPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edukonnect.edukonnect_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdukonnectPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
